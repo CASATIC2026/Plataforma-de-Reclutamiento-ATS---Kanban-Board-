adbe524e24840347c9f85bb06cfec26628c0ba43
watashiwabenny-netizen <watashiwabenny@gmail.com>
2026-03-25 09:27:31 -0600

    NEW UPDATE + BUGS TO FIX
    Signed-off-by: watashiwabenny-netizen <watashiwabenny@gmail.com>

--- RecruitmentAPI/obj/Debug/net8.0/RecruitmentAPI.AssemblyInfo.cs
+++ RecruitmentAPI/obj/Debug/net8.0/RecruitmentAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecruitmentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9aefe98ec002a5f2d898955257136884fb0c2a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73fd6ce140608201a9d06cef997ad76a967eaac0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecruitmentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecruitmentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
