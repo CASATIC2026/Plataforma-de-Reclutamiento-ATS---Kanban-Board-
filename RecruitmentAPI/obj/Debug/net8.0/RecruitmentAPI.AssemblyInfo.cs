86b9040944db1c0df6660f074f84d1e6995036ef
watashiwabenny-netizen <watashiwabenny@gmail.com>
2026-04-15 15:43:32 -0600

    INPUT VALIDATION FORM ALGORITHM IMPROVED

--- RecruitmentAPI/obj/Debug/net8.0/RecruitmentAPI.AssemblyInfo.cs
+++ RecruitmentAPI/obj/Debug/net8.0/RecruitmentAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecruitmentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73fd6ce140608201a9d06cef997ad76a967eaac0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65eedbed1cc674ea873823970abbe3b4ed1292f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecruitmentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecruitmentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
