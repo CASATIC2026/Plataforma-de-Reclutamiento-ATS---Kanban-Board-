34e7376065b9759e5868fa97d0fbd14bfe692a2e
watashiwabenny-netizen <watashiwabenny@gmail.com>
2026-05-11 10:15:21 -0600

    feat(rbac+email): wire delayed email automation backend; fix silent role assignment

--- RecruitmentAPI/obj/Debug/net8.0/RecruitmentAPI.AssemblyInfo.cs
+++ RecruitmentAPI/obj/Debug/net8.0/RecruitmentAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecruitmentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a443bfc6dfcc267c82cfa93d32d1f21e5ca1207")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b96b56711c90c99a283eb46023a9173a550ef447")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecruitmentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecruitmentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
